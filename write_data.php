30a78221ae96cf4a3c8505261c22bc081869c22b
Kit Double <kitdouble@gmail.com>
2023-05-05 14:50:27 +1000

    Add files via upload

--- write_data.php
+++ write_data.php
@@ -5,7 +5,7 @@ $data = $post_data['filedata'];
 // generate a unique ID for the file, e.g., session-6feu833950202
 $file = uniqid("session-");
 // the directory "data" must be writable by the server
-$name = "data/{$file}.csv";
+$name = "Data/{$file}.csv";
 // write the file to disk
 file_put_contents($name, $data);
 ?>
