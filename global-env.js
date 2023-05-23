fd109851024120895436ce3c07773058d8794bba
Kit Double <kitdouble@gmail.com>
2023-05-23 11:24:43 +1000

    Add files via upload

--- global-env.js
+++ global-env.js
@@ -1,41 +1,44 @@
   var welcome = {
-      type: jsPsychSurveyText,
-      questions: [{prompt: "Please enter your Prolific ID", name: 'prolificID', required:true}],
-      preamble: "<font size = '15' color='#002080'><b>Welcome to the experiment!</b></font><br><br>",
-      button_label: "Begin",
-      on_finish: function(data){
-        participant_id = data.response.prolificID;
-        jsPsych.data.addProperties({participant_id: participant_id});
-        if(PROLIFIC_PID != undefined){jsPsych.data.addProperties({PROLIFIC_PID: PROLIFIC_PID});};
-      }
-    };
+    type: jsPsychSurveyText,
+    questions: [{prompt: "Please enter your Prolific ID", name: 'prolificID', required:true}],
+    preamble: "<font size = '15' color='#002080'><b>Welcome to the experiment!</b></font><br><br>",
+    button_label: "Begin",
+    on_finish: function(data){
+      participant_id = data.response.prolificID;
+      jsPsych.data.addProperties({participant_id: participant_id});
+      if(PROLIFIC_PID != undefined){jsPsych.data.addProperties({PROLIFIC_PID: PROLIFIC_PID});};
+    }
+  };
 
 
-var demographics = {
+  var demographics = {
     type: jsPsychSurveyHtmlForm,
     preamble: "<font size = '15' color='#002080'>Basic Information</font>",
     html: '<div style = "text-align: left"><br><p><b> Age: </b><br><input name="age" type="number" min ="18" max = "100" style = "width: 8em;"required></p>' +
     '<p><b>Sex:</b></p><div><input type="radio" id="male" name="sex" value="male"unchecked><label for="male">Male</label></div><div><input type="radio" id="female" name="sex" value="female"><label for="female">Female</label></div><div><input type="radio" id="other" name="sex" value="other"><label for="other">Non binary/prefer not to say</label></div>' +
     '<p><b>Do you speak english fluently?:</b></p><div><input type="radio" id="yes" name="english" value="yes"unchecked><label for="yes">Yes</label></div><div><input type="radio" id="no" name="english" value="no"><label for="no">No</label></p></div><div></div>',
+    on_finish: function(data){
+      jsPsych.data.addProperties({age: data.response.age, sex: data.response.sex, english_fluent: data.response.english});
+    },
     on_load: function(){
-          document.querySelector('#jspsych-survey-html-form-next').disabled = true;
-           
+      document.querySelector('#jspsych-survey-html-form-next').disabled = true;
 
-            $('input[type="radio"]').on("click", function(e) {
-                  if($("input[name=age]" ).val() == '' || $("input[type=radio][name=sex]:checked" ).val() === undefined || $( "input[type=radio][name=english]:checked" ).val() === undefined ){return} else {
-                  document.querySelector('#jspsych-survey-html-form-next').disabled = false;
-                    }
-                })
 
-               $('input[type="number"]').on("change", function(e) {
-                  if($("input[name=age]" ).val() == '' || $("input[type=radio][name=sex]:checked" ).val() === undefined || $( "input[type=radio][name=english]:checked" ).val() === undefined ){return} else {
-                  document.querySelector('#jspsych-survey-html-form-next').disabled = false;
-                    }
-                })
+      $('input[type="radio"]').on("click", function(e) {
+        if($("input[name=age]" ).val() == '' || $("input[type=radio][name=sex]:checked" ).val() === undefined || $( "input[type=radio][name=english]:checked" ).val() === undefined ){return} else {
+          document.querySelector('#jspsych-survey-html-form-next').disabled = false;
+        }
+      })
 
-               
+      $('input[type="number"]').on("change", function(e) {
+        if($("input[name=age]" ).val() == '' || $("input[type=radio][name=sex]:checked" ).val() === undefined || $( "input[type=radio][name=english]:checked" ).val() === undefined ){return} else {
+          document.querySelector('#jspsych-survey-html-form-next').disabled = false;
+        }
+      })
 
-              }
+
+
+    }
   };
 
 
@@ -43,9 +46,9 @@ var demographics = {
 
 
 
-  // Save
-      function saveData(data){
-        var xhr = new XMLHttpRequest();
+  // Save to server
+  function saveDataServer(data){
+    var xhr = new XMLHttpRequest();
         xhr.open('POST', 'write_data.php'); // 
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify({filedata: data}));
@@ -56,7 +59,7 @@ var demographics = {
         type: jsPsychCallFunction,
         async: true,
         func: function(done){
-          saveData(jsPsych.data.get().csv());
+          saveDataServer(jsPsych.data.get().csv());
           done();
         }
       }
@@ -68,3 +71,11 @@ var demographics = {
         stimulus : "Saving...please wait",
         trial_duration: 4000
       }
+
+
+
+
+  
+
+
+
